ea9a0d24b6ce40a8cc1eeb55dd7613d915aa0248
shahlo-alavova <64728278+shahlo-alavova@users.noreply.github.com>
2021-08-23 12:12:44 +0600

    Add files via upload

---
+++ weather app/js.js
@@ -0,0 +1,10 @@
+var age = Number(prompt("What is your age?"))
+if(age < 0) {alert("ERROR")}
+{console.log("Nope, your age cannot be negative")};
+if(age === 21) {alert("Happy 21st birthday!")};
+{console.log("Happy 21st birthday!")};
+if(age % 2 !== 0) {alert("ODD!")};
+if(String(Math.sqrt(age)).includes(".")===false)
+	{alert("Your age is a perfect square!")}
+	{console.log("Your age is a perfect square!")};
+
